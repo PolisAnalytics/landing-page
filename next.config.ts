2a2e2498ef94447897c2f2a0396161d4a9c63975
andres suarez <andresuarezo130494@gmail.com>
2025-08-19 21:35:58 -0300

    update-fix

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    resend_key: process.env.RESEND,
     email: process.env.EMAIL,
     password: process.env.EMAIL_PASS,
-    base: "http://localhost:300"
   }
 };
 
--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -7,8 +7,8 @@ export async function POST(req: NextRequest) {
 
   const transporter = nodemailer.createTransport({
     host: "smtp.zoho.com",
-    port: 465,
-    secure: true, // true para puerto 465, false para 587
+    port: 587,
+    secure: false, // true para puerto 465, false para 587
     auth: {
       user: process.env.email,
       pass: process.env.password,
@@ -33,6 +33,6 @@ export async function POST(req: NextRequest) {
     await transporter.sendMail(mailOptions);
     return NextResponse.json({ success: true });
   } catch (error){
-    return NextResponse.json({ success: false, error }, { status: 500 });
+    return NextResponse.json({ success: false, error, name: process.env.password }, { status: 500 });
   }
 }
